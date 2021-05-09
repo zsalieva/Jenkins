de7a65228a24eb1fcb08e14c5c111548a6c81051
zsalieva <zuhrasalieva@gmail.com>
2021-05-09 15:02:20 -0500

    added backend info

--- vpc/backend.tf
+++ vpc/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-files-mcalik"
+    bucket = "terraform-state-files-zsalieva"
     key    = "tfstate/vpc-module.tfstate"
     region = "us-east-1"
   }
