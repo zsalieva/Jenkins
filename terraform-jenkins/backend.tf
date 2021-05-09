058a0b288e4822ceaa8b89dbb9177b9b2bf970ce
zsalieva <zuhrasalieva@gmail.com>
2021-05-09 15:07:54 -0500

    back

--- terraform-jenkins/backend.tf
+++ terraform-jenkins/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-files-mcalik"
+    bucket = "terraform-state-files-zsalieva"
     key    = "tfstate/jenkins.tfstate"
     region = "us-east-1"
   }
